68024bd3e9becc05343fb88d14092d67d6952c7a
Emily Oudyk <eoudyk@gmail.com>
2023-12-04 16:37:21 -0500

    Add header title and basic styling, remove default css formatting in App.css since not needed

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "../Header/Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__title">TaskTrek</div>
+    </div>
+  );
+};
+
+export default Header;
