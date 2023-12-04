5df8c6b6cea4b9cd2161fed8c71b43b49d7ead17
Emily Oudyk <eoudyk@gmail.com>
2023-12-04 16:45:39 -0500

    Add footer text and basic styling, remove formatting that placed all page contents in center of page

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import "../Footer/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__credit">Created by Emily Oudyk</div>
+      <a
+        href="https://www.eoudyk.com"
+        target="_blank"
+        className="footer__contact"
+      >
+        eoudyk.com
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
